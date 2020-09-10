a34afbd78451d192cda903b66990e6c9c9d54ec3
Roko <rokokovac98@gmail.com>
2020-09-10 10:24:32 +0200

    Clean up starting project

---
+++ Manistra.Mobile/manistra/lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:manistra/screens/home.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Manistra',
+      home: Home(),
+    );
+  }
+}
