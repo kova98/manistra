2c52a4cb255395f2402038fa463fe67854aa096f
Roko <rokokovac98@gmail.com>
2020-11-07 13:25:48 +0100

    Add MigrationManager

--- Manistra.API/Program.cs
+++ Manistra.API/Program.cs
@@ -13,7 +13,10 @@ namespace Manistra.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
