6c54ab3afa2d75d7da55995605369b548337b84e
Roko <rokokovac98@gmail.com>
2020-11-07 13:26:48 +0100

    Refactor secrets

---
+++ Manistra.API/Helpers/SecretsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manistra.API.Helpers
+{
+    public static class SecretsHelper
+    {
+        static SecretsHelper()
+        {
+            ConnectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+        }
+
+        public static string ConnectionString { get; }
+    }
+}
